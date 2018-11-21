b075ea74e9138097b8a7a7878a0112920a8f2322
Ivan G. Rojas <ivangr21@gmail.com>
2018-11-20 19:20:35 -0500

    add new component

---
+++ src/Components/newPath.jsx
@@ -0,0 +1,89 @@
+import {React} from 'react'
+import {ReactDOM} from 'react-dom'
+import {route} from 'react-router-dom'
+
+class Blah extends Component {
+    render() {
+        return(
+            <div>
+                <body>
+    <header>
+        <nav class="primary-menu">
+            <ul>
+                <li>Home</li>
+                <li>Resources</li>
+                <li>Get Involved</li>
+                <li>Founder Bios</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+        <div class="branding"><p class="heading1">Logo</p></div>
+    </header>
+    <section class="hero">
+        <p class="heading2">
+                We are a grassroots effort focused on
+                fostering diversity and inclusion in Pasco
+                County schools and community.
+        </p>
+        <p>
+            [ Client’s “Value Statement” document ] Together, our commitment to diversity, inclusion, and equity has the potential to positively disrupt and transform entrenched practices and thinking. And as a result, we will hold schools in Land O’Lakes, Wesley Chapel, New Port Richey, and the PascoCountySchoolSystemasawhole, accountabletoagoalofgraduatingstudentswhoareengagedworldcitizens,withadiscerning intelligence, an understanding of inequality and power, and moral courage.
+        </p>
+        <button>Donate Now</button>
+    </section>
+    <section class="community">
+        <h3>Community</h3>
+        <div class="box">
+            <img src="https://via.placeholder.com/150" alt="place holder image">
+            <h4>Teacher Story</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit neque aspernatur necessitatibus id iusto, ab ex doloremque corporis adipisci, culpa obcaecati explicabo itaque recusandae, blanditiis sequi iure odio fugiat. Obcaecati!</p>
+        </div>
+        <div class="box">
+                <img src="https://via.placeholder.com/150" alt="place holder image">
+                <h4>Event Story</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit neque aspernatur necessitatibus id iusto, ab ex doloremque corporis adipisci, culpa obcaecati explicabo itaque recusandae, blanditiis sequi iure odio fugiat. Obcaecati!</p>
+        </div>
+        <div class="box">
+                <img src="https://via.placeholder.com/150" alt="place holder image">
+                <h4>Parent Story</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit neque aspernatur necessitatibus id iusto, ab ex doloremque corporis adipisci, culpa obcaecati explicabo itaque recusandae, blanditiis sequi iure odio fugiat. Obcaecati!</p>
+        </div>
+    </section>
+    <section class="press">
+        <div class="links">
+            <h4>Press</h4>
+            <ul class="clippings">
+                <li>
+                    <span class="heading">Agents for Change push...</span> 
+                    <p>http://www.baynews9.com/fl/ tampa/news/2018/06/19/-pasco-county- agents-for-change--push-for-diversity-- inclusion-training-in-schools. </p>
+                </li>
+                <li>
+                        <span class="heading">Pasco Moms ask School...</span> 
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit neque aspernatur necessitatibus id iusto, ab ex doloremque corporis adipisci,</p>
+                    </li>
+            </ul>
+        </div>
+        <div class="latest-photo">
+            <img src="https://via.placeholder.com/600" alt="place holder image">
+        </div>
+    </section>
+    <footer>
+        <div class="branding">LOGO</div>
+        <button>Donate Now</button>
+        <menu class="bottom-list">
+            <ul>
+                <li>Bios</li>
+                <li>Press</li>
+                <li>Contact Us</li>
+                <li>Resources</li>
+                <li>Donate</li>
+                <li>Founder</li>
+                <li>Connect With Us</li>
+                <li>Facebook</li>
+            </ul>
+        </menu>
+    </footer>
+</body>
+            </div>
+        )
+}
+}
