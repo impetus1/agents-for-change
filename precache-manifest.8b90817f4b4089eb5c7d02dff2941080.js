5ffc6618ac0fb4b6751e45c00c2efdb67b06bf10
impetus1 <ivangr21@gmail.com>
2019-03-12 20:27:56 -0400

    Updates

---
+++ precache-manifest.8b90817f4b4089eb5c7d02dff2941080.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f69d7dfbff8d9b47e32e",
+    "url": "/agents-for-change/static/css/main.ef52407b.chunk.css"
+  },
+  {
+    "revision": "f69d7dfbff8d9b47e32e",
+    "url": "/agents-for-change/static/js/main.8e42644c.chunk.js"
+  },
+  {
+    "revision": "2b1c484b3dd22a69ea76",
+    "url": "/agents-for-change/static/js/runtime~main.caf03811.js"
+  },
+  {
+    "revision": "5ffd3bee2f2add272455",
+    "url": "/agents-for-change/static/css/2.42e0fa66.chunk.css"
+  },
+  {
+    "revision": "5ffd3bee2f2add272455",
+    "url": "/agents-for-change/static/js/2.9d4a86b2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/agents-for-change/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "582bdb9cac6e25e35e0616c62666b1fb",
+    "url": "/agents-for-change/static/media/navbar.582bdb9c.png"
+  },
+  {
+    "revision": "e56cdef8ce9d1983730c1283766e362a",
+    "url": "/agents-for-change/index.html"
+  }
+];
